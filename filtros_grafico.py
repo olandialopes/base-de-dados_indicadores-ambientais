300f8e307b476b0c45d3a46024b543571c15785a
EmanuelFirmino <ddos.emanuel@gmail.com>
2024-03-25 21:22:40 -0300

    :chart_with_upwards_trend: New box plots :heavy_plus_sign:

--- filtros_grafico.py
+++ filtros_grafico.py
@@ -5,6 +5,32 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pickle
+import pandas as pd
+import os
+
+# Colors to use on boxplots
+color_palette = {
+    'Manufacturing': 'red',
+    'Trade': 'purple',
+    'Utilities': 'green',
+    'Transport': 'grey',
+    'Mining': 'yellow',
+    'Government': 'tomato',
+    'Construction': 'orange',
+    'Business': 'midnightblue',
+    'Agriculture': 'navy',
+    'OtherServices': 'lawngreen',
+    'RealEstate': 'gold',
+    'Financial': 'pink'
+}
+
+color_region = {
+    'Sudeste': 'blue',
+    'Nordeste': 'orange',
+    'Norte': 'green',
+    'Centro-oeste': 'brown',
+    'Sul': 'purple'
+}
 
 
 indicadores = ['quant_efluentes_liquidos',
@@ -22,47 +48,78 @@ def count_unique_firms(base):
     return num_firms
 
 
-def plot_boxplot(data, x='ano', y='quant_tonelada', number=1, region='', setor='', poluente=''):
-    sns.boxplot(data=data, x=x, y=y, whis=(0, 100))
-    plt.title(f'{x}_{y}_{region}_{setor}_{poluente}'.capitalize())
-    plt.xticks(rotation=90)
-    plt.tight_layout(pad=2.0)
-    plt.savefig(f'plots_td/boxplot_{number}_{region}_{setor}_{poluente}.png')
-    plt.show()
+def plot_boxplot(data, x='isic_12', y='quant_tonelada',
+                number=1, region='', poluente='',
+                title = 'Quantidade poluentes / Setores',
+                ylim_superior = 200, ylim_inferior = 1,
+                ylabel='Quantidade poluente (Mg)',
+                xlabel='Setores', pallete=color_palette):
+    
+    if region or poluente:
+        title += '\n'
+    if region:
+        title += f'{region}'
+    if poluente:
+        title += f' - {poluente}'
+    
+    sns.boxplot(data=data, x=x, y=y, palette=pallete)
+    
+    plt.title(title)
+    plt.ylim((ylim_inferior, ylim_superior))
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.xticks(rotation=45)
+    plt.tight_layout(pad=1.0)
+
+    title = f'boxplot_{number}'
+
+    if region:
+        title += f'_{region}'
+    if poluente:
+        title += f'_{poluente}'
+    title += '.png'
+
+    # Made file path more flexible on different operational systems.
+    plt.savefig(os.path.join('plots_td', title))
+#    plt.show()
     plt.close()
 
 
 def gera_plots(data):
     number = 1
-    # grafico 1 TD - residuos solidos acima de 1 tonelada - setor realestate
+
+    # Gráfico 1 TD - Resíduos sólidos acima de 1 tonelada - setor realestate
     key = 'residuos_solidos2'
     indicador = 'quant_tonelada'
-    setor = 'RealEstate'
     minimum = 1
     year = 2011
     base1 = data[key][(data[key][indicador] > minimum) &
-                      (data[key]['isic_12'] == setor) &
                       (data[key]['ano'] > year)]
     plot_boxplot(base1, y=indicador, number=number)
     number += 1
+    #########################################################################
 
-    # grafico 2 TD - residuos solidos acima de 26000 toneladas - setor transport/trade
-    for setor in ['Transport', 'Trade']:
-        minimum = 26000
-        base2 = data[key][(data[key][indicador] > minimum) &
-                          (data[key]['isic_12'] == setor)]
-        plot_boxplot(base2, y=indicador, number=number)
-        number += 1
+    # Gráfico 2 TD - resíduos sólidos acima de 26000 toneladas - setor transport/trade
+    
+    minimum = 26000
+    base2 = data[key][(data[key][indicador] > minimum)]
+    base2 = base2[(base2['isic_12'] == 'Transport') | (base2['isic_12'] == 'Trade')]
+    plot_boxplot(base2, y=indicador, number=number, ylim_inferior=40000, ylim_superior=1000000)
+    number += 1
 
-    # Gráfico 4 TD - poluentes atmosféricos por setores econômicos acima de zero. Mesmas keys para regiões
+    #########################################################################
+
+    # Gráfico 4 TD - Poluentes atmosféricos por setores econômicos acima de zero. Mesmas keys para regiões
     key = 'poluentes_atm'
     indicador = 'quant_poluentes_emitidos'
     minimum = 0
     base4 = data[key][(data[key][indicador] > minimum)]
-    plot_boxplot(base4, y=indicador, number=number)
+    plot_boxplot(base4, y=indicador, number=number, 
+                 title='Poluentes atmosféricos / Setores econômicos',
+                 ylim_superior=5000)
     number += 1
 
-    # gráfico - teste para separar o tipo de poluente atmosférico:
+    # Gráfico - Teste para separar o tipo de poluente atmosférico:
     regions = ['Sudeste', 'Norte', 'Sul', 'Centro-oeste', 'Nordeste']
     key = 'poluentes_atm'
     indicador = 'quant_poluentes_emitidos'
@@ -71,14 +128,14 @@ def gera_plots(data):
                         'Óxidos de nitrogênio (NOx)',
                         'Óxidos de enxofre (SOx)']
     for region in regions:
-        for setor in data[key]['isic_12'].unique():
-            for poluente in poluente_emitido:
-                base = data[key][(data[key]['tipo_poluente'] == poluente) &
-                                 (data[key]['isic_12'] == setor) &
-                                 (data[key]['region'] == region)]
-                if len(base) > 0:
-                    plot_boxplot(base, y=indicador, number=number, region=region, setor=setor, poluente=poluente)
-                    number += 1
+        for poluente in poluente_emitido:
+            base = data[key][(data[key]['tipo_poluente'] == poluente) &
+                             (data[key]['region'] == region)]
+            if len(base) > 0:
+                plot_boxplot(base, y=indicador, number=number,
+                             region=region, poluente=poluente,
+                             ylim_superior=3500)
+                number += 1
 
     # Gráfico 5 a 9 e 10 a 14 TD - poluentes atmosféricos por setores econômicos e por região (valores acima de zero)
 
@@ -86,29 +143,38 @@ def gera_plots(data):
         for region in regions:
             base = data[key][(data[key][indicador] > minimum) &
                              (data[key]['region'] == region)]
-            plot_boxplot(base, y=indicador, number=number, region=region)
+            plot_boxplot(base, y=indicador,
+                        number=number, region=region,
+                        ylim_superior=30000,
+                        title='Poluentes atmosféricos / Setores econômicos')
             number += 1
 
-    # grafico 15 TD - indicador eficiência de tratamento de efluentes por setor econômico (valores >0 e <100)
+    # Gráfico 15 TD - Indicador eficiência de tratamento de efluentes por setor econômico (valores >0 e <100).
     key = 'efluentes'
     indicador = 'perc_efficiency_treatment'
     value = [0, 100]
     base = data[key][(data[key][indicador] > min(value)) &
                      (data[key][indicador] < max(value))]
-    plot_boxplot(base, y=indicador, number=number)
+    plot_boxplot(base, y=indicador, number=number,
+                 ylim_inferior=1, ylim_superior=100,
+                 title='Indicador eficiência efluentes / Setor',
+                 ylabel='Indicador eficiência efluentes (0 < valor < 100)')
     number += 1
+    ##########################################################################################################
 
-    # grafico 16 TD - efluentes liquidos  acima de 9000 m3/h - nacional
+
+    # Gráfico 16 TD - Efluentes líquidos  acima de 9000 m3/h - nacional
     key = 'efluentes'
     indicador = 'quant_efluentes_liquidos'
     minimum = 9000
     year = 2010
     base = data[key][(data[key][indicador] > minimum) &
-                     (data[key]['isic_12'] == setor) &
-                     (data[key]['ano'] > year) &
-                     (data[key]['region'] == region)]
+                     (data[key]['ano'] > year)]
     if len(base) > 0:
-        plot_boxplot(base, y=indicador, number=number)
+        plot_boxplot(base, y=indicador, number=number,
+                     ylim_inferior=minimum, ylim_superior=50000,
+                     title = 'Efluentes líquidos / Setores',
+                     ylabel='Efluentes líquidos (m3/h)')
         number += 1
 
     # análise da proporcionalidade da poluição por região
@@ -120,10 +186,12 @@ def gera_plots(data):
                 sum_indicador = data[key].groupby(by=['ano', 'region'])[indicador].agg('sum').reset_index()
                 quantidade_empresas[key]['razao'] = sum_indicador[indicador] / quantidade_empresas[key][0]
                 quantidade_empresas[key].to_csv(f'output/quantidade_empresas_{key}.csv', index=False)
-                for region in regions:
-                    plot_boxplot(quantidade_empresas[key][quantidade_empresas[key]['region'] == region],
-                                 y='razao', number=number, region=region)
-                    number += 1
+                
+                plot_boxplot(quantidade_empresas[key],
+                                y='razao', number=number, ylabel='Indicador por região',
+                                x='region', pallete=color_region, ylim_inferior=200, ylim_superior=150000,
+                                title='Indicador / Região', xlabel='Regiões')
+                number += 1
 
 
 if __name__ == '__main__':
